bda1f1b7c1b3be96aad3fae6797d6d376a85d631
mariondek10 <marion.dekerchove@unil.ch>
2026-04-22 18:32:48 +0200

    test code M5flow

--- data.py
+++ data.py
@@ -28,3 +28,26 @@ query_job = client.query(q)
 
 df = query_job.to_dataframe()
 print(df)
+
+
+
+#M5 flow python code
+'''
+setScreenColor(0x29aa54)
+gps_0 = unit.get(unit.GPS, unit.PORTC)
+
+gps_long = M5TextBox(179, 85, "label0", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+longText = M5TextBox(88, 85, "Longitude :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+lat_text = M5TextBox(88, 119, "Latitude :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+gps_lat = M5TextBox(179, 119, "label2", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+
+date = M5TextBox(88, 150, "Date", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+
+
+gps_0.uart_port_id(1)
+gps_long.setText(str(gps_0.longitude))
+gps_lat.setText(str(gps_0.latitude))
+date.setText(str(gps_0.gps_date))
+
+print(gps_0.latitude)
+'''
