875262ecb4215e920ea4ec7556b0df2372b90b86
mariondek10 <marion.dekerchove@unil.ch>
2026-05-05 16:32:56 +0200

    change title

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,16 +1,23 @@
 import streamlit as st
 
 def main():
-    st.title("🚴‍♀️ Welcome to your biking Dashboard!")
-    st.write("Here is an overview of your last biking session:")
+    st.set_page_config(
+    page_title="Bike Dashboard",
+    page_icon="🚲",
+)
+    st.title("🚲 Welcome to your biking Dashboard!")
+    st.write("Here is an overview of all your biking session:")
     st.map()
+    st.subheader("Your Recent Biking Session:")
+    # Display a selecter that allows the user to choose a time range for their biking sessions
+    st.selectbox("Select a time range for your biking sessions:", options=["Last Week", "Last Month", "Last Year"])
     st.dataframe({
         "Distance (km)": [15.2],
         "Duration (min)": [45],
         "Average Speed (km/h)": [20.3],
         "Calories Burned": [350]
     })
-    
+
 if __name__ == "__main__":
     main()
 
